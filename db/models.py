f0432cb91069a3547460b57762f4dbd91df25f7f
Roman Kostiv <rkostiv@gmail.com>
2025-04-30 18:41:55 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import UniqueConstraint
 
 
 class Genre(models.Model):
@@ -17,7 +20,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
@@ -50,3 +53,52 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class User(AbstractUser):
+    pass
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to=User, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return f"Order: {self.created_at.strftime("%d/%m/%Y %H:%M:%S")}"
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(to=MovieSession, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(to=Order, on_delete=models.DO_NOTHING)
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self) -> str:
+        return (f"Ticket: "
+                f"{self.movie_session.movie.title} "
+                f"{self.order.created_at.strftime('%d/%m/%Y %H:%M:%S')}"
+                f"(row: {self.row}, seat: {self.seat})")
+
+
+    class Meta:
+        constraints = [UniqueConstraint(fields=["row", "seat", "movie_session"], name="unique_ticket")]
+
+
+    def clean(self) -> None:
+        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+            raise ValidationError(
+                {"seat": f"Seat must be between 1 and "
+                         f"{self.movie_session.cinema_hall.seats_in_row}, not {self.seat}"
+                 }
+            )
+        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+            raise ValidationError(
+                {"row": f"Row must be between 1 and "
+                        f"{self.movie_session.cinema_hall.seats_in_row}, not {self.row}"
+                 }
+            )
+
+
+    def save(self, *args, **kwargs) -> None:
+        super.full_clean()
+        return super().save(*args, **kwargs)
---
+++ services/order.py
@@ -0,0 +1,20 @@
+from django.db import transaction
+from db.models import MovieSession, Ticket, User, Order
+from typing import List, Dict
+
+
+def create_order(tickets: List[Dict], username: str, date: str = None):
+    with transaction.atomic():
+        user = User.objects.get(username=username)
+        order = Order.objects.create(user=user, created_at=date)
+        for ticket in tickets:
+            Ticket.objects.create(
+                order=order,
+                row=ticket["row"],
+                seat=ticket["seat"],
+                movie_session=ticket["movie_session"]
+            )
+
+
+def get_orders(username: str):
+    return Order.objects.filter(username__contains=username).order_by('-id')
